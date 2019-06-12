5bfcab6aa69d6d7d53d16df90b165f9ad3fbb4a4
Sandeep Parkhande <sandeep.parkhande@gmail.com>
2019-06-11 21:30:31 -0400

    Commited by Example 3 for conflicts

--- src/Example1.java
+++ src/Example1.java
@@ -2,6 +2,8 @@
 public class Example1 {
 
 	public static void main(String[] args) {
+		System.out.print(" Commited by Git Example 3");
+		System.out.println("Why Commit in Git Example 1");
 		System.out.println(" Why Commit Example1 in Git Example 1");
 	}
 }
